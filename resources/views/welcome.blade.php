6e782d8fd07d55452417a8b7a37aae95f0c6c517
t.goto <t.goto@chatbox-inc.com>
2018-07-13 00:55:20 +0900

    change_title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PHPCON2018!
                 </div>
 
                 <div class="links">
